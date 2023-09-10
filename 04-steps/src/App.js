6968747844bf8f26f6ab5975ef273eb7d6e5d813
Dawid Zych <dawid.aliens1989@gmail.com>
2023-09-10 13:31:48 +0200

    dodanie drugiego state na X

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -4,6 +4,7 @@ export default function App() {
 	const messages = ['Learn React ⚛️', 'Apply for jobs 💼', 'Invest your new income 🤑'];
 
 	const [step, setStep] = useState(1);
+	const [isOpen, setIsOpen] = useState(true);
 
 	function handlePreviouse() {
 		if (step > 1) setStep(step - 1);
@@ -11,26 +12,34 @@ export default function App() {
 	function handleNext() {
 		if (step < 3) setStep(step + 1);
 	}
+
 	return (
-		<div className='steps'>
-			<div className='numbers'>
-				<div className={step >= 1 ? 'active' : ''}>1</div>
-				<div className={step >= 2 ? 'active' : ''}>2</div>
-				<div className={step >= 3 ? 'active' : ''}>3</div>
-			</div>
+		<div>
+			<button className='close' onClick={() => setIsOpen(!isOpen)}>
+				&times;
+			</button>
+			{isOpen && (
+				<div className='steps'>
+					<div className='numbers'>
+						<div className={step >= 1 ? 'active' : ''}>1</div>
+						<div className={step >= 2 ? 'active' : ''}>2</div>
+						<div className={step >= 3 ? 'active' : ''}>3</div>
+					</div>
 
-			<p className='message'>
-				Step {step}: {messages[step - 1]}
-			</p>
+					<p className='message'>
+						Step {step}: {messages[step - 1]}
+					</p>
 
-			<div className='buttons'>
-				<button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePreviouse}>
-					Previous
-				</button>
-				<button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext}>
-					Next
-				</button>
-			</div>
+					<div className='buttons'>
+						<button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePreviouse}>
+							Previous
+						</button>
+						<button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext}>
+							Next
+						</button>
+					</div>
+				</div>
+			)}{' '}
 		</div>
 	);
 }
