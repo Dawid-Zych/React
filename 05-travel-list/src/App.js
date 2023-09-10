ce708091bab77e0deb176aeaf5b454b83387a40f
Dawid Zych <dawid.aliens1989@gmail.com>
2023-09-10 18:46:35 +0200

    lista i formularz

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -3,12 +3,18 @@ import Form from './Components/Form.js';
 import Stats from './Components/Stats.js';
 import PackingList from './Components/PackingList.js';
 
+const initialItems = [
+	{ id: 1, description: 'Passports', quantity: 2, packed: false },
+	{ id: 2, description: 'Socks', quantity: 12, packed: true },
+	{ id: 3, description: 'Charger', quantity: 1, packed: false },
+];
+
 export default function App() {
 	return (
 		<div className='app'>
 			<Logo />
 			<Form />
-			<PackingList />
+			<PackingList initial={initialItems} />
 			<Stats />
 		</div>
 	);
--- 05-travel-list/src/Components/PackingList.js
+++ 05-travel-list/src/Components/PackingList.js
@@ -1,3 +1,22 @@
-export default function PackingList() {
-	return <div className='list'>LIST</div>;
+export default function PackingList({ initial }) {
+	return (
+		<div className='list'>
+			<ul>
+				{initial.map(item => (
+					<Item item={item} key={item.id} />
+				))}
+			</ul>
+		</div>
+	);
+}
+
+function Item({ item }) {
+	return (
+		<li>
+			<span style={item.packed ? {textDecoration: 'line-throught'} : {}}>
+				{item.quantity} {item.description}
+			</span>
+			<button>❌</button>
+		</li>
+	);
 }
