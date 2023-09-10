9cdbde6bccf6a0e907499bf616190654760e91be
Dawid Zych <dawid.aliens1989@gmail.com>
2023-09-10 13:06:52 +0200

    creating step variable in useState

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,17 +1,22 @@
+import { useState } from 'react';
+
 export default function App() {
 	const messages = ['Learn React ⚛️', 'Apply for jobs 💼', 'Invest your new income 🤑'];
 
-	let step = 1;
+	const [step, setStep] = useState(1);
 
-	function handlePreviouse() {}
-	function handleNext() {}
-	console.log(step);
+	function handlePreviouse() {
+		if (step > 1) setStep(step - 1);
+	}
+	function handleNext() {
+		if (step < 3) setStep(step + 1);
+	}
 	return (
 		<div className='steps'>
 			<div className='numbers'>
-				<div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-				<div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-				<div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+				<div className={step >= 1 ? 'active' : ''}>1</div>
+				<div className={step >= 2 ? 'active' : ''}>2</div>
+				<div className={step >= 3 ? 'active' : ''}>3</div>
 			</div>
 
 			<p className='message'>
