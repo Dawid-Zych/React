6031c026d9b91f4a2d83182f79a464e3250fea77
Dawid Zych <dawid.aliens1989@gmail.com>
2023-09-13 17:27:04 +0200

    deleting items

--- 05-travel-list/src/Components/PackingList.js
+++ 05-travel-list/src/Components/PackingList.js
@@ -1,27 +1,27 @@
-export default function PackingList({ items }) {
-	const initialItems = [
+export default function PackingList({ items, onDeleteItems }) {
+	/* 	const initialItems = [
 		{ id: 1, description: 'Passports', quantity: 2, packed: false },
 		{ id: 2, description: 'Socks', quantity: 12, packed: true },
 		{ id: 3, description: 'Charger', quantity: 1, packed: false },
-	];
+	]; */
 	return (
 		<div className='list'>
 			<ul>
 				{items.map(item => (
-					<Item item={item} key={item.id} />
+					<Item item={item} key={item.id} onDeleteItems={onDeleteItems} />
 				))}
 			</ul>
 		</div>
 	);
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItems }) {
 	return (
 		<li>
 			<span style={item.packed ? { textDecoration: 'line-throught' } : {}}>
 				{item.quantity} {item.description}
 			</span>
-			<button>❌</button>
+			<button onClick={() => onDeleteItems(item.id)}>❌</button>
 		</li>
 	);
 }
