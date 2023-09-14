a3054ab547af96957d3cd7d9fa8d93f32dc8f144
Dawid Zych <dawid.aliens1989@gmail.com>
2023-09-14 11:46:35 +0200

    statystyki

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -7,7 +7,7 @@ import { useState } from 'react';
 export default function App() {
 	const initialItems = [
 		{ id: 1, description: 'Passports', quantity: 2, packed: false },
-		{ id: 2, description: 'Socks', quantity: 12, packed: true },
+		{ id: 2, description: 'Socks', quantity: 12, packed: false },
 		{ id: 3, description: 'Charger', quantity: 1, packed: false },
 	];
 	const [items, setItems] = useState(initialItems);
@@ -29,7 +29,7 @@ export default function App() {
 			<Logo />
 			<Form onAddItems={handleAddItems} />
 			<PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
-			<Stats />
+			<Stats items={items} />
 		</div>
 	);
 }
