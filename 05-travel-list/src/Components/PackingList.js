0403e0584f8e03aab04dbaba080ae14113dce29d
Dawid Zych <dawid.aliens1989@gmail.com>
2023-09-14 12:05:17 +0200

    sortowanie

--- 05-travel-list/src/Components/PackingList.js
+++ 05-travel-list/src/Components/PackingList.js
@@ -1,11 +1,29 @@
+import { useState } from 'react';
+
 export default function PackingList({ items, onDeleteItem, onToggleItem }) {
+	const [sortBy, setSortBy] = useState('input');
+
+	let sortedItems;
+
+	if (sortBy === 'input') sortedItems = items;
+	if (sortBy === 'description')
+		sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description));
+	if (sortBy === 'packed') sortedItems = items.toSorted((a, b) => Number(a.packed) - Number(b.packed));
 	return (
 		<div className='list'>
 			<ul>
-				{items.map(item => (
+				{sortedItems.map(item => (
 					<Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} />
 				))}
 			</ul>
+
+			<div className='action'>
+				<select value={sortBy} onChange={e => setSortBy(e.target.value)}>
+					<option value='input'>Sort by input order</option>
+					<option value='description'>Sort by description</option>
+					<option value='packed'>Sort by packed status</option>
+				</select>
+			</div>
 		</div>
 	);
 }
