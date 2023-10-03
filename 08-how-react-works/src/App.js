fca1332d095ff767a81196502c38226e7bae8186
Dawid Zych <dawid.aliens1989@gmail.com>
2023-10-03 12:19:08 +0200

    finish how react works

--- 08-how-react-works/src/App.js
+++ 08-how-react-works/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const content = [
 	{
@@ -27,7 +27,7 @@ export default function App() {
 }
 
 // console.log(<DifferentContent test = {23}/>); // widzi component instance
-console.log(DifferentContent()); // widzi react element
+// console.log(DifferentContent()); // widzi react element
 
 function Tabbed({ content }) {
 	const [activeTab, setActiveTab] = useState(0);
@@ -65,8 +65,36 @@ function TabContent({ item }) {
 	const [showDetails, setShowDetails] = useState(true);
 	const [likes, setLikes] = useState(0);
 
+	console.log('RENDER');
+
 	function handleInc() {
-		setLikes(likes + 1);
+		setLikes(likes => likes + 1);
+	}
+
+	function handleTripleInc() {
+		// not work
+		// setLikes(likes + 1);
+		// setLikes(likes + 1);
+		// setLikes(likes + 1);
+
+		// work
+		// setLikes(likes => likes + 1);
+		// setLikes(likes => likes + 1);
+		// setLikes(likes => likes + 1);
+
+		handleInc();
+		handleInc();
+		handleInc();
+	}
+
+	function handleUndo() {
+		setShowDetails(true);
+		setLikes(0);
+		console.log(likes);
+	}
+
+	function handleUndoLater() {
+		setTimeout(handleUndo, 2000);
 	}
 
 	return (
@@ -80,13 +108,13 @@ function TabContent({ item }) {
 				<div className='hearts-counter'>
 					<span>{likes} ❤️</span>
 					<button onClick={handleInc}>+</button>
-					<button>+++</button>
+					<button onClick={handleTripleInc}>+++</button>
 				</div>
 			</div>
 
 			<div className='tab-undo'>
-				<button>Undo</button>
-				<button>Undo in 2s</button>
+				<button onClick={handleUndo}>Undo</button>
+				<button onClick={handleUndoLater}>Undo in 2s</button>
 			</div>
 		</div>
 	);
