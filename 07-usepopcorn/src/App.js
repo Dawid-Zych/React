1a865693e0f6f805990958996aa1d83450aa00be
Dawid Zych <dawid.aliens1989@gmail.com>
2023-10-04 12:31:11 +0200

    wylapywanie bledow

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -49,16 +49,29 @@ export default function App() {
 	const [movies, setMovies] = useState([]);
 	const [watched, setWatched] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
-	const query = 'harry potter';
+	const [error, setError] = useState('');
+
+	const query = 'adadad';
 
 	useEffect(function () {
 		async function fetchMovies() {
-			setIsLoading(true);
-			const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s={${query}}`);
+			try {
+				setIsLoading(true);
+				const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s={${query}}`);
 
-			const data = await res.json();
-			setMovies(data.Search);
-			setIsLoading(false);
+				if (!res.ok) throw new Error('Something get wrong with fetching movies');
+
+				const data = await res.json();
+				if (data.Response === 'False') throw new Error('Movie not found!');
+
+				setMovies(data.Search);
+				console.log(data);
+			} catch (err) {
+				console.error(err.message);
+				setError(err.message);
+			} finally {
+				setIsLoading(false);
+			}
 		}
 
 		fetchMovies();
@@ -73,7 +86,12 @@ export default function App() {
 			</NavBar>
 
 			<Main>
-				<Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
+				<Box>
+					{/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
+					{isLoading && <Loader />}
+					{!isLoading && !error && <MovieList movies={movies} />}
+					{error && <ErrorMessage message={error} />}
+				</Box>
 
 				<Box>
 					<WatchedSummary watched={watched} />
@@ -86,6 +104,15 @@ export default function App() {
 
 function Loader() {
 	return <p className='loader'>Loading...</p>;
+}
+
+function ErrorMessage({ message }) {
+	return (
+		<p className='error'>
+			{' '}
+			<span>✋</span> {message}
+		</p>
+	);
 }
 
 function NavBar({ children }) {
