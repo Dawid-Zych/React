1456d37b25bf51c91fc901ee35d712e0e5c7dfb3
Dawid Zych <dawid.aliens1989@gmail.com>
2023-10-17 14:34:47 +0200

    komunikacja rodzic dziecko

--- 09-classy-weather/src/App.js
+++ 09-classy-weather/src/App.js
@@ -68,17 +68,13 @@ class App extends React.Component {
 		}
 	};
 
+	setLocation = e => this.setState({ location: e.target.value });
+
 	render() {
 		return (
 			<div className='app'>
 				<h1>Classy Weather</h1>
-				<div>
-					<input
-						type='text'
-						placeholder='Search for location...'
-						value={this.state.location}
-						onChange={e => this.setState({ location: e.target.value })}></input>
-				</div>
+				<Input location={this.state.location} onChangeLocation={this.setLocation} />
 				<button onClick={this.fetchWeather}>Get weather</button>
 
 				{this.state.isLoading && <p className='loader'>Loading....</p>}
@@ -86,6 +82,20 @@ class App extends React.Component {
 				{this.state.weather.weathercode && (
 					<Weather weather={this.state.weather} location={this.state.displayLocation} />
 				)}
+			</div>
+		);
+	}
+}
+
+class Input extends React.Component {
+	render() {
+		return (
+			<div>
+				<input
+					type='text'
+					placeholder='Search for location...'
+					value={this.props.location}
+					onChange={this.props.onChangeLocation}></input>
 			</div>
 		);
 	}
