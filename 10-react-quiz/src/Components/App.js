23376f0203e2a99a22ef754a3cad651b0dd556e0
Dawid Zych <dawid.aliens1989@gmail.com>
2023-10-19 15:55:53 +0200

    React quiz app component

--- 10-react-quiz/src/Components/App.js
+++ 10-react-quiz/src/Components/App.js
@@ -1,12 +1,16 @@
-import DateCounter from './DateCounter';
 import Error from './Error';
 import Loader from './Loader';
 import Header from './Header';
+import Main from './Main';
 
 export default function App() {
 	return (
-		<div className='App'>
-			<DateCounter />
+		<div className='app'>
+			<Header />
+			<Main>
+				<p>1/15</p>
+				<p>Question?</p>
+			</Main>
 		</div>
 	);
 }
