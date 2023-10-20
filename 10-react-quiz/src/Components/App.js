37e35f774998fbb8aa8684413a103adcb4025391
Dawid Zych <dawid.aliens1989@gmail.com>
2023-10-20 18:17:59 +0200

    finish a quiz

--- 10-react-quiz/src/Components/App.js
+++ 10-react-quiz/src/Components/App.js
@@ -7,6 +7,7 @@ import StartScreen from './StartScreen';
 import Questions from './Questions';
 import NextButton from './NextButton';
 import Progress from './Progress';
+import FinishScreen from './FinishScreen';
 
 const initialState = {
 	questions: [],
@@ -16,6 +17,7 @@ const initialState = {
 	index: 0,
 	answer: null,
 	points: 0,
+	highscore: 0,
 };
 
 function reducer(state, action) {
@@ -50,6 +52,12 @@ function reducer(state, action) {
 				index: state.index + 1,
 				answer: null,
 			};
+		case 'finish':
+			return {
+				...state,
+				status: 'finished',
+				highscore: state.points > state.highscore ? state.points : state.highscore,
+			};
 
 		default:
 			throw new Error('Action unknown');
@@ -57,7 +65,7 @@ function reducer(state, action) {
 }
 
 export default function App() {
-	const [{ questions, status, index, answer, points }, dispatch] = useReducer(reducer, initialState);
+	const [{ questions, status, index, answer, points, highscore }, dispatch] = useReducer(reducer, initialState);
 
 	const numQuestions = questions.length;
 	const maxPossiblePoints = questions.reduce((prev, curr) => prev + curr.points, 0);
@@ -100,8 +108,12 @@ export default function App() {
 							answer={answer}
 						/>
 						<Questions question={questions[index]} dispatch={dispatch} answer={answer} />
-						<NextButton dispatch={dispatch} answer={answer} />
+						<NextButton dispatch={dispatch} answer={answer} index={index} numQuestions={numQuestions} />
 					</>
+				)}
+
+				{status === 'finished' && (
+					<FinishScreen maxPossiblePoints={maxPossiblePoints} points={points} highscore={highscore} />
 				)}
 			</Main>
 		</div>
