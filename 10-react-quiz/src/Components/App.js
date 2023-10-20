8668b545d6730902ecb87ff87b3996eab3c65851
Dawid Zych <dawid.aliens1989@gmail.com>
2023-10-20 17:34:19 +0200

    next question

--- 10-react-quiz/src/Components/App.js
+++ 10-react-quiz/src/Components/App.js
@@ -5,6 +5,7 @@ import Header from './Header';
 import Main from './Main';
 import StartScreen from './StartScreen';
 import Questions from './Questions';
+import NextButton from './NextButton';
 
 const initialState = {
 	questions: [],
@@ -40,9 +41,13 @@ function reducer(state, action) {
 			return {
 				...state,
 				answer: action.payload,
-				points: action.payload === question.correctOption 
-				  ? state.points + question.points 
-				  : state.points,
+				points: action.payload === question.correctOption ? state.points + question.points : state.points,
+			};
+		case 'nextQuestion':
+			return {
+				...state,
+				index: state.index + 1,
+				answer: null,
 			};
 
 		default:
@@ -83,7 +88,12 @@ export default function App() {
 				{status === 'loading' && <Loader />}
 				{status === 'error' && <Error />}
 				{status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
-				{status === 'active' && <Questions question={questions[index]} dispatch={dispatch} answer={answer} />}
+				{status === 'active' && (
+					<>
+						<Questions question={questions[index]} dispatch={dispatch} answer={answer} />
+						<NextButton dispatch={dispatch} answer={answer} />
+					</>
+				)}
 			</Main>
 		</div>
 	);
--- 10-react-quiz/src/Components/Options.js
+++ 10-react-quiz/src/Components/Options.js
@@ -1,11 +1,12 @@
 function Options({ question, dispatch, answer }) {
 	const hasAnswered = answer !== null;
+	console.log(hasAnswered);
 	return (
 		<div className='options'>
 			{question.options.map((option, index) => (
 				<button
 					className={`btn btn-option ${index === answer ? 'answer' : ''} ${
-						hasAnswered && index === question.correctOption ? 'correct' : 'wrong'
+						hasAnswered ? (index === question.correctOption ? 'correct' : 'wrong') : ''
 					} `}
 					key={option}
 					disabled={hasAnswered}
