343ce66919de1d54947ab176ad015b424c902f84
Dawid Zych <dawid.aliens1989@gmail.com>
2023-10-23 14:07:25 +0200

    upd

--- 10-react-quiz/src/Components/App.js
+++ 10-react-quiz/src/Components/App.js
@@ -78,6 +78,7 @@ function reducer(state, action) {
 				...state,
 				secondsRemaining: state.secondsRemaining - 1,
 				status: state.secondsRemaining === 0 ? 'finished' : state.status,
+				highscore: state.secondsRemaining === 0 ? Math.max(state.points, state.highscore) : state.highscore,
 			};
 
 		default:
