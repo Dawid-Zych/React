1974ae6a42e2eeade7b04e01f985b8be4324057c
Dawid Zych <dawid.aliens1989@gmail.com>
2023-10-25 13:56:56 +0200

    WorldWise vite project start

---
+++ 11-worldwise/src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
+
+/* W wczesniejszych wersiach vite nie było eslinta trzeba było go zainstalować
+  npm install eslint vite-plugin-eslint eslint-config-react-app --save-dev
+nastepnie tworzymy plik .eslintrc.json
+
+{ "extends": "react-app"}
+
+następnie w pliku vite.config.js 
+import eslint from "vite-plugin-eslint"
+
+i dodajemy go do naszego exportu
+
+export default defineConfig({
+  plugins: [react(),eslint()],
+})
+
+*/
