de7bfcb59dc4750b45afc3628f3119d367933f36
Dawid Zych <dawid.aliens1989@gmail.com>
2024-01-18 18:17:00 +0100

    fetching data with react router loaders

--- 15 fast-react-pizza/src/features/menu/MenuItem.jsx
+++ 15 fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,18 +1,23 @@
-function MenuItem({ pizza }) {
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+import { formatCurrency } from '../../utils/helpers';
+import PropTypes from 'prop-types';
 
-  return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
-        <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
-        </div>
-      </div>
-    </li>
-  );
+MenuItem.propTypes = {
+	pizza: PropTypes.object,
+};
+
+function MenuItem({ pizza }) {
+	const { name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+	return (
+		<li>
+			<img src={imageUrl} alt={name} />
+			<div>
+				<p>{name}</p>
+				<p>{ingredients.join(', ')}</p>
+				<div>{!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}</div>
+			</div>
+		</li>
+	);
 }
 
 export default MenuItem;
